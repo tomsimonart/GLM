ed5aca01e7703ffb2c001a999f454b806f1060d9
infected <tom.simonart@gmail.com>
2016-02-07 11:57:47 +0100

    Initial Commit

---
+++ example_plugin.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+from os import system
+from time import sleep
+from stream_utils.streamtools import Stream
+
+class ExamplePlugin(Stream):
+    def __init__(self):
+        # Credits - Version
+        self.name = 'Demo Plugin'
+        self.author = 'Infected'
+        self.version = 'beta 0.4'
+
+        # Stream initialization
+        Stream.__init__(self, height=16, width=64)
+
+        # Stream data
+        self.data1 = ''.join([str((i+1)%2) for i in range(1024)])
+        self.data2 = ''.join([str(i%2) for i in range(1024)])
+        self.buffer_end = 0
+        self.delay = 0.4
+
+        # First data assignation (not necessary)
+        Stream.set_data_from_string(self, self.data2)
+
+    def stream(self):
+        """Used to send the data to a Stream object
+        stream(self) is the main loop of a plugin"""
+        while self.buffer_end == 0:
+            Stream.set_data_from_raw(self, self.data1)
+            print(self, end='')
+            sleep(self.delay)
+            system('clear')
+
+            Stream.set_data_from_raw(self, self.data2)
+            print(self, end='')
+            sleep(self.delay)
+            system('clear')
+
+    def get_info(self):
+        """Get the current state and information of the plugin"""
+        print(self.name, self.author, self.version, sep='\n')
+        print(self.__repr__())
+        print(self,end='')
+        print(bytes(self))
+
+plugin = ExamplePlugin()
+plugin.stream()
+# plugin.get_info()
---
+++ stream_utils/streamtools.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# StreamTools by Infected
+
+from serial import Serial
+
+class Stream():
+
+    def __init__(self, height=16, width=64):
+        self.height = height
+        self.width = width
+        self.lenght = height * width
+        self.byte = bytes(1)
+        # Empty initial data
+        self.data = ''.join(['0' for i in range(self.height * self.width)])
+
+    def __str__(self):
+        display = ''
+        if self.data != None:
+            n = 0
+            for i in range(self.height):
+                for j in range(self.width):
+                    if self.data[n] == '1':
+                        display += "\033[41m \033[0m"
+                    else:
+                        display += "\033[44m \033[0m"
+                    n += 1
+                display += '\n'
+        else:
+            display = 'StreamError: No data to display'
+        return display
+
+    def __repr__(self):
+        return 'Stream(' + str(self.height) + ', ' + str(self.width) + ')'
+
+    def __bytes__(self):
+        byte_list = [int(self.data[i:i+8],2)for i in range(0,self.lenght-1,8)]
+        return bytes(byte_list)
+
+    def set_size(self, height, width):
+        self.height = height
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_data_from_raw(self, data):
+        """Fastest solution if the data is in a stripped string format"""
+        self.data = data
+
+    def set_data_from_matrix(self, data):
+        matrix = ''.join([str(j) for i in data for j in i])
+        self.data = matrix
+
+    def set_data_from_string(self, data):
+        self.data = data.replace('\n', '').strip()
+
+    def set_data_from_list(self, data):
+        self.data = ''.join(map(str, data))
+
+    def get_data(self):
+        return self.data
+
+    def send_to_serial(self):
+        pass
+
+    def refresh(self):
+        self.lenght = self.height * self.width
+
+    def __del__(self):
+        pass
