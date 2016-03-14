fd99d3825be998ef30d65d1b0ee18fbf876c63f1
infected <tom.simonart@gmail.com>
2016-03-14 01:50:52 +0100

    [new] crepebot plugin

---
+++ crepebot_plugin.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Crepe bot plugin by infected
+
+from libs.streamtools import Stream
+from libs.textlib import Text
+from libs.image import Image
+from libs import pbmtools
+
+class CrepeBot:
+    def __init__(self):
+        self.percentage = 0
+        self.pbm
