ad8924ad634db0c5438a1d3642305658f08b5e03
Minorias <pavel_polinetskyy@hotmail.com>
2016-03-15 22:15:23 +0100

    [fix] Off by one error in pixmap updating [fix] Canvas/cursor discrepancy

--- drawing_plugin.py
+++ drawing_plugin.py
@@ -2,6 +2,10 @@
 
 import tkinter
 
+# CLEAR BUTTON
+# INSTRUCTIONS
+# FILL ALL BUTTON
+
 
 class MatrixDrawer:
     def __init__(self, x=64, y=16):
@@ -11,21 +15,24 @@ class MatrixDrawer:
         self.x = x
         self.y = y
 
+        self.create_canvas()
+
         self.root.bind("<Button-1>", self.mouseinteract1)
         self.root.bind("<B1-Motion>", self.mouseinteract2)
         self.root.bind("<Button-3>", self.mouseinteract3)
         self.root.bind("<B3-Motion>", self.mouseinteract3)
 
-        self.create_canvas()
         self.create_buttons()
 
     def create_canvas(self):
+        self.canvasframe = tkinter.Frame(self.root)
+        self.canvasframe.pack(side="right")
         square_size = 22
-        self.canvas = tkinter.Canvas(self.root, bg="yellow",
+        self.canvas = tkinter.Canvas(self.canvasframe, bg="yellow",
                                      height=len(self.pixmap)*square_size,
                                      width=len(self.pixmap[1])*square_size)
 
-        self.canvas.grid(row=0, column=1, rowspan=20)
+        self.canvas.grid(row=0, column=0)
 
         for y in range(self.y):
             for x in range(self.x):
@@ -35,7 +42,9 @@ class MatrixDrawer:
                                              fill="grey")
 
     def create_buttons(self):
-        self.terminalbutton = tkinter.Button(self.root,
+        self.buttonframe = tkinter.Frame(self.root)
+        self.buttonframe.pack(side="left")
+        self.terminalbutton = tkinter.Button(self.buttonframe,
                                              text="Send To Matrix",
                                              bg="Yellow",
                                              command=self.update_pixmap
@@ -43,44 +52,52 @@ class MatrixDrawer:
         self.terminalbutton.grid(row=0, column=0)
 
     def mouseinteract1(self, event):
-        """For Left-click
-           Write/Erase
         """
-        x = self.canvas.canvasx(event.x)
-        y = self.canvas.canvasy(event.y)
+        For Left-click
+        Write/Erase
+        """
+        if event.widget.winfo_id() == self.canvas.winfo_id():
+            x = self.canvas.canvasx(event.x)
+            y = self.canvas.canvasy(event.y)
 
-        square = self.canvas.find_closest(x, y)
+            square = self.canvas.find_closest(x, y)
 
-        if self.canvas.itemcget(square, "fill") == "grey":
-            self.canvas.itemconfig(square, fill="red")
-        else:
-            self.canvas.itemconfig(square, fill="grey")
+            if self.canvas.itemcget(square, "fill") == "grey":
+                self.canvas.itemconfig(square, fill="red")
+            else:
+                self.canvas.itemconfig(square, fill="grey")
 
     def mouseinteract2(self, event):
-        """For Left-Drag
-           Write
         """
-        x = self.canvas.canvasx(event.x)
-        y = self.canvas.canvasy(event.y)
+        For Left-Drag
+        Write
+        """
+        if event.widget.winfo_id() == self.canvas.winfo_id():
+            x = self.canvas.canvasx(event.x)
+            y = self.canvas.canvasy(event.y)
 
-        square = self.canvas.find_closest(x, y)
-        self.canvas.itemconfig(square, fill="red")
+            square = self.canvas.find_closest(x, y)
+            self.canvas.itemconfig(square, fill="red")
 
     def mouseinteract3(self, event):
-        """For Right-click / Right-Drag
-           Erase
         """
-        x = self.canvas.canvasx(event.x)
-        y = self.canvas.canvasy(event.y)
+        For Right-click / Right-Drag
+        Erase
+        """
+        if event.widget.winfo_id() == self.canvas.winfo_id():
+            x = self.canvas.canvasx(event.x)
+            y = self.canvas.canvasy(event.y)
 
-        square = self.canvas.find_closest(x, y)
-        self.canvas.itemconfig(square, fill="grey")
+            square = self.canvas.find_closest(x, y)
+            self.canvas.itemconfig(square, fill="grey")
 
     def update_pixmap(self):
         for i in range(self.x * self.y):
-            if self.canvas.itemcget(i, "fill") == "red":
+            if self.canvas.itemcget(i+1, "fill") == "red":
                 self.pixmap[i // 64][i % 64] = 1
-        print(self.pixmap)
+
+        for elem in self.pixmap:
+            print(elem)
 
 a = MatrixDrawer()
 a.root.mainloop()
