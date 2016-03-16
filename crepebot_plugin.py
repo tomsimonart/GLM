2f64883242e033c3c385579358ce197ab1ca32d9
infected <tom.simonart@gmail.com>
2016-03-16 17:52:53 +0100

    [fix] compatible with screen

--- crepebot_plugin.py
+++ crepebot_plugin.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Crepe bot plugin by infected
 
-from libs.streamtools import Stream
+from libs.screen import Screen
 from libs.text import Text
 from libs.image import Image
 from libs import pbmtools
@@ -14,12 +14,18 @@ from autobahn.asyncio.wamp import ApplicationSession, ApplicationRunner
 class CrepeBot(ApplicationSession):
     def __init__(self, *args,**kwargs):
         super(CrepeBot, self).__init__(*args, **kwargs)
+        self.screen = Screen(matrix=True, show=True)
         self.pbm = Image()
         self.bar = Image()
+        self.splash = Text('init v1.0')
+        self.percentage_text = Text('0')
         self.bar_x = 40
+
         self.bar_y = 7
-        self.image = Image()
-        self.stream = Stream(matrix=True)
+        self.screen.add(self.pbm, 0, 0, False)
+        self.screen.add(self.bar, self.bar_x, self.bar_y, True)
+        self.screen.add(self.splash, 25, 0, True)
+        self.screen.add(self.percentage_text, 40, 10, True)
 
     @coroutine
     def onJoin(self, details):
@@ -35,17 +41,10 @@ class CrepeBot(ApplicationSession):
         self.subscribe(onRefresh, 'queue')
 
     def refresh(self, percentage, name):
-        self.splash = Text(str(name).lower())
-        self.image.blank()
-        self.bar.blank()
-        self.image.paste(self.splash, x=25, y=0)
+        self.splash.edit(str(name).lower)
         self.refresh_bar(percentage)
-        self.image.paste(self.bar.get_pixmap(), x=self.bar_x, y=self.bar_y)
-        self.percentage_text = Text(str(percentage) + '%')
-        self.image.paste(self.percentage_text, x=40, y=10)
-        self.stream.set_data_from_matrix(self.image.get_pixmap())
-        self.stream.send_to_serial()
-        #print(self.stream)
+        self.percentage_text.edit(str(percentage) + '%')
+        self.screen.refresh()
 
     def refresh_bar(self, percentage):
         self.bar.draw_line(x1=0, y1=0, x2=percentage // 5, y2=0)
