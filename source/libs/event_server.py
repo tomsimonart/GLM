0b41add73a2924e85ddcb5dae4b083c36254f539
infected <tom.simonart@gmail.com>
2018-02-15 16:49:19 +0100

    [add] event server

---
+++ source/libs/event_server.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+class EventServer():
+    """docstring for EventServer."""
+    def __init__(self):
+        super(EventServer, self).__init__()
--- source/plugins/web_plugin.py
+++ source/plugins/web_plugin.py
@@ -8,10 +8,10 @@ class Plugin():
         self.name = "Example Plugin"
         self.version = "0.0.3"
 
-        self.sample_text = Text('sample text')
+        self.sample_text = Text('web plugin')
 
         self.screen = Screen(matrix=matrix, show=show)
-        self.screen.add(self.sample_text, refresh=False)
+        self.screen.add(self.sample_text, refresh=False, x=9, y=4)
 
         self.interface = Webframe()
 
