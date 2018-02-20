eec893e7722445740eafc7dcf896985d8684a375
infected <tom.simonart@gmail.com>
2018-02-20 18:14:44 +0100

    [what] did i do?

--- source/libs/webclient.py
+++ source/libs/webclient.py
@@ -21,29 +21,29 @@ class WebClient():
     def send_data(self, user="plugin", data=" "):
         connected = False
         failed = False
-        while not connected and not failed:
-            self.client.send(user.encode()) # 1 send (user name)
-            server_recv = self.client.recv(BUFFSIZE).decode()
-            if server_recv == "a:client_connected":
-                # Client connected
-                connected = True
-                msg("Connected", 0, "send_data", server_recv)
-                self.client.send("data".encode()) # 2 send data
+        # while not connected and not failed:
+        #     self.client.send(user.encode()) # 1 send (user name)
+        #     server_recv = self.client.recv(BUFFSIZE).decode()
+        #     if server_recv == "a:client_connected":
+        #         # Client connected
+        #         connected = True
+        #         msg("Connected", 0, "send_data", server_recv)
+        #         self.client.send("data".encode()) # 2 send data
+        #
+        #     elif server_recv == "e:bad_user":
+        #         # Failed to connect
+        #         msg("Failed to connect", 2, "send_data", server_recv)
+        #         failed = True
+        #
+        #     else:
+        #         # Failed to connect
+        #         msg("No connection, trying again...", 2)
+        #         sleep(1)
 
-            elif server_recv == "e:bad_user":
-                # Failed to connect
-                msg("Failed to connect", 2, "send_data", server_recv)
-                failed = True
-
-            else:
-                # Failed to connect
-                msg("No connection, trying again...", 2)
-                sleep(1)
-
-        if connected:
-            # async send recv
-            ans = self.client.recv(BUFFSIZE)
-            msg(ans.decode(), 0, "server")
+        # if connected:
+        #     # async send recv
+        #     ans = self.client.recv(BUFFSIZE)
+        #     msg(ans.decode(), 0, "server")
 
         msg("Closing socket", 1)
         self.client.close()
