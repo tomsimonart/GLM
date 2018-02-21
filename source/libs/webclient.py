65544cf658400988f87e97453853e0d4020966c7
infected <tom.simonart@gmail.com>
2018-02-21 19:56:33 +0100

    [dev]

--- source/libs/webclient.py
+++ source/libs/webclient.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import json
 import socket
 import signal
 from time import sleep
@@ -13,37 +14,35 @@ class WebClient():
         super(WebClient, self).__init__()
         self.server_port = server_port
         self.server_ip = server_ip
+
+        # Catch process closing
+        signal.signal(signal.SIGTERM, self.close_connection)
         self.kill = False
 
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((self.server_ip, server_port))
 
+    def close_connection(self, signum, frame):
+        self.kill = True
+
     def send_data(self, user="plugin", data=" "):
-        connected = False
-        failed = False
-        # while not connected and not failed:
-        #     self.client.send(user.encode()) # 1 send (user name)
-        #     server_recv = self.client.recv(BUFFSIZE).decode()
-        #     if server_recv == "a:client_connected":
-        #         # Client connected
-        #         connected = True
-        #         msg("Connected", 0, "send_data", server_recv)
-        #         self.client.send("data".encode()) # 2 send data
-        #
-        #     elif server_recv == "e:bad_user":
-        #         # Failed to connect
-        #         msg("Failed to connect", 2, "send_data", server_recv)
-        #         failed = True
-        #
-        #     else:
-        #         # Failed to connect
-        #         msg("No connection, trying again...", 2)
-        #         sleep(1)
+        self.client.send(user.encode()) # Send user name
+        response = self.client.recv(BUFFSIZE).decode()
+        msg(response, 0, "plugin_handler")
 
-        # if connected:
-        #     # async send recv
-        #     ans = self.client.recv(BUFFSIZE)
-        #     msg(ans.decode(), 0, "server")
+        if response == "a:client_connected":
+            event = self.client.recv(BUFFSIZE).decode()
+            msg(data, 1, "Event")
+            self.client.send(data.encode()) # Sending data
 
-        msg("Closing socket", 1)
-        self.client.close()
+        else:
+            msg("Connection refused", 3)
+
+        while not self.kill:
+            if self.kill:
+                msg("killed", 3, "Process")
+                self.client.send(b"EOT") # Sending end signal
+                self.client.close()
+            else:
+                event = json.dumps(self.client.recv(BUFFSIZE).decode())
+                msg("event", 0, "plugin_handler", event)
